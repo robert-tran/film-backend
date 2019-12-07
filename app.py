bbd3a52a30dd226b258f192b2b32a5b790b745db
Tran Thanh Sang <42944073+robert-tran@users.noreply.github.com>
2019-12-07 18:16:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def create_app(
 
 if __name__ == '__main__':
     application = create_app()
-    application.run((host='127.0.0.1', port=8080, debug=True)
+    application.run(host='127.0.0.1', port=8080, debug=True)
